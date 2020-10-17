4b5301abe6e1c53c32e12d8ce61034ff91fb034d
Stefany-Souza <70451854+Stefany-Souza@users.noreply.github.com>
2020-10-17 18:47:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const ano = prompt('Em qual ano você nasceu? ')
+const nome = prompt('E qual é o seu nome? ')
+
+const anoAtual = '2020'
+    
+dados = anoAtual - ano;     
+
+alert(`Olá! Seu nome é ${nome} e você tem ${dados} anos.`);
